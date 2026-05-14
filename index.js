1491ad297f2cc23c4562e102696a483e1ce3b281
Aminwsf <154097506+Aminwsf@users.noreply.github.com>
2026-05-14 23:24:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 //const sharp = require("sharp");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/trtimg", async (req, res) => {
   const { url, lang = "eng+ara+fra+spa+chis_sim+jpn+kor" } = req.query;
